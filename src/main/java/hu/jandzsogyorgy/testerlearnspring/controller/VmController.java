11c4df1a477f8f6022beabb7dfa4cd84a45ce868
György Jandzsó <haver1036@gmail.com>
2024-12-11 10:40:49 +0100

    Bugfix: Wrong node

--- src/main/java/hu/jandzsogyorgy/testerlearnspring/controller/VmController.java
+++ src/main/java/hu/jandzsogyorgy/testerlearnspring/controller/VmController.java
@@ -1,6 +1,7 @@
 package hu.jandzsogyorgy.testerlearnspring.controller;
 
 import hu.jandzsogyorgy.testerlearnspring.dto.VmDto;
+import hu.jandzsogyorgy.testerlearnspring.service.NodeService;
 import hu.jandzsogyorgy.testerlearnspring.service.VmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,22 @@ import java.util.List;
 @RequestMapping(value = "/api/pve/nodes/{node}/vms", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class VmController {
+    private final NodeService nodeService;
     private final VmService vmService;
+
+    private boolean validateNode(String node) {
+        try {
+            return nodeService.getNode(node) != null;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
     @GetMapping
     public List<VmDto> getVms(@PathVariable String node) {
-        return vmService.listAllVm(node);
+        if(validateNode(node)){
+            return vmService.listAllVm(node);
+        }
+        return List.of();
     }
 }
